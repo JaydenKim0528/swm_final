cf8e41b88bf8cedc66a675cb0fac66b432c7aee5
Heymi Ha <hmss002@naver.com>
2024-07-30 14:40:37 +0900

    feat: register css, html 추가

---
+++ src/main/java/kr/co/swm/member/controller/MemberController.java
@@ -0,0 +1,15 @@
+package kr.co.swm.member.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+    @GetMapping("/login.do")
+    public String login() {
+        return "member/login";
+    }
+}

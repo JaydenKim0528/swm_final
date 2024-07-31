633b82b033ade37a48990729a6135e41073f9e9a
jinwoo <dlwlsdn8022@gmail.com>
2024-07-31 17:45:56 +0900

    feat: 예약하기 페이지에 들어가는 기본적인 틀 제작

--- src/main/java/kr/co/swm/SwmApplication.java
+++ src/main/java/kr/co/swm/SwmApplication.java
@@ -1,7 +1,12 @@
-package main.java.kr.co.swm;
+package kr.co.swm;
 
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class SwmApplication {
 
 	public static void main(String[] args) {
